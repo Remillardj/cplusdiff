c7a5af3ea7039ce1141612ed057df8ee5e1a7752
Grant Kalfus <32502685+Grant-Kalfus@users.noreply.github.com>
2018-01-31 23:00:15 -0800

    Added some comments.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
+//Very very very inefficient program to remove multi word instances
+//Could have been a lot easier with regular expressions, or another programming lang, but I digress
+
 using namespace std;
 #include<string>
 #include<iostream>
 #include<fstream>
 
-
+//compares two words to see if they are the same
 string word_compare(string words, int &end_location) {
 
 	int twospace = 0;
@@ -21,6 +24,8 @@ string word_compare(string words, int &end_location) {
 	return words;
 }
 
+//returns a string with two words in it
+//offset used for a start position to look for the next two words
 string get_two_words(string format, int &offset) {
 	int temp = 0;
 
@@ -51,22 +56,36 @@ string get_two_words(string format, int &offset) {
 	return ret_string;
 }
 
-
+//main script execution 
 int main() {
+	//input and output files
 	ifstream in_file("sample.txt");
 	ofstream out_file("output.txt");
+	
+	//used for multi-line text files
 	string text = "";
+	
+	//string to hold the word(s) to replace
 	string replace_word = "";
+	
+	//used for the split in to two words function
 	int offset = 0;
+	
+	//used to tell the program where to put the end of the replacement
 	int end_location;
+	
+	//used to redefine the max length of the line
 	int full_length = 0;
 
 	while (!in_file.eof()) {
 		getline(in_file, text);
 		text += '\n';
 		full_length = text.length();
+		//loop for splitting and analyzing two words, removing one if duplicate is found
 		for (int i = 0; i < full_length - 1;) {
+			//get the words to replace
 			replace_word = word_compare(get_two_words(text, i), end_location);
+			//replace them with the ones in the main string
 			text.replace(offset, end_location, replace_word);
 			offset = i;
 			full_length = text.length();
